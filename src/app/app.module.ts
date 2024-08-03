ef82115079d27304b5badafe594278ce12e2781b
suriya <drsuriyakannan@gmail.com>
2024-08-03 11:39:40 +0530

    added chat and message for live chat to speech

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SpeechConverterComponent } from './speech-converter/speech-converter.component';
+import { ChatComponent } from './chat/chat.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SpeechConverterComponent
+    SpeechConverterComponent,
+    ChatComponent,
+    MessageComponent,
   ],
   imports: [
     BrowserModule,
